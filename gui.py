1270fe8a917483cc959c0b83be07e0cc58b0bd6e
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2023-11-04 09:33:59 +0700

    add gui.py to move pygame related code there

---
+++ gui.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+# window setup
+DISPLAYSURF = pygame.display.set_mode((800, 600))
+ALPHASURF = DISPLAYSURF.convert_alpha()
+pygame.display.set_caption('Wordsmith - Prapat edition')
+
+# Simple sound effects
+TIC = pygame.mixer.Sound('media/tic.ogg')
+TICTIC = pygame.mixer.Sound('media/tictic.ogg')
+DINGDING = pygame.mixer.Sound('media/dingding.ogg')
+SCRIFFLE = pygame.mixer.Sound('media/scriffle.ogg')
+CLICK = pygame.mixer.Sound('media/click.ogg')
