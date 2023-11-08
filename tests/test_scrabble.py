0c6663f5b3d73a9d54e9620b51b2696637a6f51c
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2023-11-08 08:40:25 +0700

    add testing stub for pytest

---
+++ tests/test_scrabble.py
@@ -0,0 +1,10 @@
+import pytest
+from scrabble import ScrabbleGame  # replace with your actual module and class
+
+# Contents of test_module1.py
+class TestScrabbleGame:
+    def test_method1(self):
+        # Test method1 of MyClass
+        pass
+
+
