70851c2f33deae79217f8dcb8fd9d16a15a40bbf
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2023-11-10 13:54:56 +0700

    Add square_model.py

--- models/square_model.py
+++ models/square_model.py
@@ -1,4 +1,9 @@
 class Square:
     """represent square in Scrabble board"""
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tile) -> None:
+        self.tile = tile
+
+class Normal_Square(Square):
+    """represent normal square in Scrabble board"""
+    def __init__(self, tile) -> None:
+        super().__init__(tile)
