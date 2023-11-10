42e11859b01bd299aa29aadce296399f56c717fe
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2023-11-10 14:06:04 +0700

    Add more test

--- models/square_model.py
+++ models/square_model.py
@@ -7,3 +7,23 @@ class Normal_Square(Square):
     """represent normal square in Scrabble board"""
     def __init__(self, tile) -> None:
         super().__init__(tile)
+
+class DoubleWord_Square(Square):
+    """represent double word square in Scrabble board"""
+    def __init__(self, tile) -> None:
+        super().__init__(tile)
+
+class TripleWord_Square(Square):
+    """represent triple word square in Scrabble board"""
+    def __init__(self, tile) -> None:
+        super().__init__(tile)
+
+class DoubleLetter_Square(Square):
+    """represent double letter square in Scrabble board"""
+    def __init__(self, tile) -> None:
+        super().__init__(tile)
+
+class TripleLetter_Square(Square):
+    """represent triple letter square in Scrabble board"""
+    def __init__(self, tile) -> None:
+        super().__init__(tile)        
--- tests/test_square.py
+++ tests/test_square.py
@@ -1,8 +1,43 @@
 import pytest
 import sys
-sys.path.append('..')
-from models.square_model import Square
+
+sys.path.append("..")
+
+from models.square_model import (
+    Square,
+    Normal_Square,
+    DoubleWord_Square,
+    TripleWord_Square,
+    DoubleLetter_Square,
+    TripleLetter_Square,
+)
+
 
 def test_square():
-    square = Square('A')
-    assert square.tile == 'A'
+    square = Square("A")
+    assert square.tile == "A"
+
+
+def test_normal_square():
+    square = Normal_Square("A")
+    assert square.tile == "A"
+
+
+def test_double_word_square():
+    square = DoubleWord_Square("A")
+    assert square.tile == "A"
+
+
+def test_triple_word_square():
+    square = TripleWord_Square("A")
+    assert square.tile == "A"
+
+
+def test_double_letter_square():
+    square = DoubleLetter_Square("A")
+    assert square.tile == "A"
+
+
+def test_triple_letter_square():
+    square = TripleLetter_Square("A")
+    assert square.tile == "A"
