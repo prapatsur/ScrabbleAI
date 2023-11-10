d07d6396577a124daef027403694ac76fe9da8d1
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2023-11-10 20:59:53 +0700

    Clean test_board.py

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,7 +1,6 @@
 import pytest
 from board import Board
 from tile import Tile
-import copy
 
 def test_all_same_column():
     board = Board()
@@ -93,38 +92,3 @@ def test_can_place():
     assert board.can_place(3, 0) == True
     # but cannot place in column 3 and row 3
     assert board.can_place(3, 3) == False
-
-
-# def test_played_words_are_broken_1():
-#     # This test is for those that are not broken
-#     board = Board()
-#     # Set up the squares attribute
-#     board.squares = [[None for _ in range(15)] for _ in range(15)]
-#     # There is a word in the first row
-#     board.squares[1][1] = ('A', 'player1')
-#     board.squares[2][1] = ('N', 'player1')
-#     board.squares[3][1] = ('T', 'player1')    
-#     # same row not broken
-#     board.squares[4][1] = ('S', 'player1')
-#     board.squares[5][1] = ('A', 'player1')
-#     assert board.played_words_are_broken([(4,1), (5,1)]) == False
-    # same row but broken
-    # assert board.played_words_are_broken([(1, 1), (2, 1), (3, 1), (5,1)]) == True
-    # assert board.played_words_are_broken([(1, 1), (1, 2), (1, 3)]) == True  
-
-# def test_played_words_are_broken_2():
-#     # This test is for those that are  broken
-#     board = Board()
-#     # Set up the squares attribute
-#     board.squares = [[None for _ in range(15)] for _ in range(15)]
-#     # There is a word in the first row
-#     board.squares[1][1] = ('A', 'player1')
-#     board.squares[2][1] = ('N', 'player1')
-#     board.squares[3][1] = ('T', 'player1')    
-#     # same row not broken
-#     board.squares[4][1] = ('S', 'player1')
-#     board.squares[6][1] = ('A', 'player1')
-#     assert board.played_words_are_broken([(4,1), (6,1)]) == True
-    # same row but broken
-    # assert board.played_words_are_broken([(1, 1), (2, 1), (3, 1), (5,1)]) == True
-    # assert board.played_words_are_broken([(1, 1), (1, 2), (1, 3)]) == True  
