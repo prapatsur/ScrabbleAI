4605a62aeeff414bc18d8ddff6814806226d48ac
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2023-11-10 19:42:07 +0700

    Add test_tile_equality

--- tests/test_tile.py
+++ tests/test_tile.py
@@ -2,6 +2,13 @@ import pytest
 from tile import Tile
 
 def test_tile_creation():
-    tile = Tile('A', 1)
+    tile = Tile(char='A', pts=1)
     assert tile.letter == 'A'
     assert tile.points == 1
+
+def test_tile_equality():
+    tile1 = Tile('A', 1)
+    tile2 = Tile('A', 1)
+    tile3 = Tile('B', 2)
+    assert tile1 == tile2
+    assert tile1 != tile3
