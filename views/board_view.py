032fedd1c554922cff7bee49338e5802ac398e9e
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2023-11-12 10:41:38 +0700

    Extract methods

--- views/board_view.py
+++ views/board_view.py
@@ -12,19 +12,69 @@ class BoardView:
         self.letter_prompt_view = LetterPromptView(self.DISPLAYSURF, self.ALPHASURF)      
 
     def draw(self):
+        self.draw_each_square()
+        self.draw_lock_shading()
+
+    def draw_lock_shading(self):
+        # =======DRAW LOCK SHADING==========
+        self.ALPHASURF.fill((0, 0, 0, 0))
+        top = self.board.BOARD_TOP
+        left = self.board.BOARD_LEFT
+        right = (
+                self.board.GRID_SIZE * (self.board.SQUARE_BORDER + self.board.SQUARE_SIZE)
+                + self.board.SQUARE_BORDER
+        )
+        bottom = (
+                self.board.GRID_SIZE * (self.board.SQUARE_BORDER + self.board.SQUARE_SIZE)
+                + self.board.SQUARE_BORDER
+        )
+        x1 = (
+                self.board.columnLock * (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER)
+                + self.board.BOARD_LEFT
+        )
+        x2 = x1 + (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER) + \
+             self.board.SQUARE_BORDER
+        y1 = self.board.rowLock * (self.board.SQUARE_SIZE +
+                                   self.board.SQUARE_BORDER) + self.board.BOARD_LEFT
+        y2 = y1 + (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER) + \
+             self.board.SQUARE_BORDER
+        if self.board.rowLock >= 0 and self.board.columnLock >= 0:
+            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
+                             (left, top, x1 - left, y1 - top))
+            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
+                             (left, y2, x1 - left, bottom - y2))
+            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
+                             (x2, top, right - x2, y1 - top))
+            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
+                             (x2, y2, right - x2, bottom - y2))
+        elif self.board.rowLock >= 0:
+            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
+                             (left, top, right - left, y1 - top))
+            pygame.draw.rect(
+                self.ALPHASURF, MASK_COLOR, (left, y2, right - left, bottom - y2)
+            )
+        elif self.board.columnLock >= 0:
+            pygame.draw.rect(
+                self.ALPHASURF, MASK_COLOR, (left, top, x1 - left, bottom - top)
+            )
+            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
+                             (x2, top, right - x2, bottom - top))
+        self.DISPLAYSURF.blit(self.ALPHASURF, (0, 0))
+
+    def draw_each_square(self):
         # draw each square
         for x in range(self.board.GRID_SIZE):
             for y in range(self.board.GRID_SIZE):
                 # draw position
                 left = (
-                    x * (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER)
-                    + self.board.SQUARE_BORDER
-                    + self.board.BOARD_LEFT
+                        x * (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER)
+                        + self.board.SQUARE_BORDER
+                        + self.board.BOARD_LEFT
                 )
                 top = (
-                    y * (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER)
-                    + self.board.SQUARE_BORDER
-                    + self.board.BOARD_TOP
+                        y * (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER)
+                        + self.board.SQUARE_BORDER
+                        + self.board.BOARD_TOP
                 )
 
                 tile = self.board.get_tile(x, y)
@@ -53,52 +103,6 @@ class BoardView:
                     else:
                         highlight = True
                     tile.draw(left, top, self.DISPLAYSURF, highlight)
-
-        # =======DRAW LOCK SHADING==========
-        self.ALPHASURF.fill((0, 0, 0, 0))
-        top = self.board.BOARD_TOP
-        left = self.board.BOARD_LEFT
-        right = (
-            self.board.GRID_SIZE * (self.board.SQUARE_BORDER + self.board.SQUARE_SIZE)
-            + self.board.SQUARE_BORDER
-        )
-        bottom = (
-            self.board.GRID_SIZE * (self.board.SQUARE_BORDER + self.board.SQUARE_SIZE)
-            + self.board.SQUARE_BORDER
-        )
-        x1 = (
-            self.board.columnLock * (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER)
-            + self.board.BOARD_LEFT
-        )
-        x2 = x1 + (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER) + \
-            self.board.SQUARE_BORDER
-        y1 = self.board.rowLock * (self.board.SQUARE_SIZE +
-                             self.board.SQUARE_BORDER) + self.board.BOARD_LEFT
-        y2 = y1 + (self.board.SQUARE_SIZE + self.board.SQUARE_BORDER) + \
-            self.board.SQUARE_BORDER
-        if self.board.rowLock >= 0 and self.board.columnLock >= 0:
-            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
-                             (left, top, x1 - left, y1 - top))
-            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
-                             (left, y2, x1 - left, bottom - y2))
-            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
-                             (x2, top, right - x2, y1 - top))
-            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
-                             (x2, y2, right - x2, bottom - y2))
-        elif self.board.rowLock >= 0:
-            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
-                             (left, top, right - left, y1 - top))
-            pygame.draw.rect(
-                self.ALPHASURF, MASK_COLOR, (left, y2, right - left, bottom - y2)
-            )
-        elif self.board.columnLock >= 0:
-            pygame.draw.rect(
-                self.ALPHASURF, MASK_COLOR, (left, top, x1 - left, bottom - top)
-            )
-            pygame.draw.rect(self.ALPHASURF, MASK_COLOR,
-                             (x2, top, right - x2, bottom - top))
-
-        self.DISPLAYSURF.blit(self.ALPHASURF, (0, 0))
 
     def draw_letter_prompt(self):
         """ Draws a letter prompt to ask for the blank letter """
