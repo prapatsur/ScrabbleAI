22680d83ea3b00d7afcfa932ce13e9d0d77c27c4
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2023-11-12 19:57:31 +0700

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,38 @@
+# settings.py
+# TODO: this file is a stub created by ChatGPT, we'll need to modify it later
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Frames per second
+FPS = 30
+
+# Colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+DARK_GRAY = (50, 50, 50)
+LIGHT_BLUE = (173, 216, 230)
+
+# Game settings
+TILE_SIZE = 40  # Size of the tiles in pixels
+BOARD_SIZE = 15  # Scrabble board is 15x15 tiles
+RACK_LENGTH = 7  # Number of tiles in a player's rack
+
+# Fonts
+FONT_NAME = 'arial'
+FONT_SIZE = 20
+
+# Paths to assets
+ASSET_PATH = 'assets/'
+IMAGE_PATH = ASSET_PATH + 'images/'
+SOUND_PATH = ASSET_PATH + 'sounds/'
+FONT_PATH = ASSET_PATH + 'fonts/'
+
+# Sound settings
+BG_MUSIC_VOLUME = 0.5
+
+# Other settings
+HIGH_SCORE_FILE = 'data/highscores.dat'
+DICTIONARY_FILE = 'data/dictionary.txt'
